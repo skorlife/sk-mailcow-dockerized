a02fab351cdbeaf186c79592b08c5e7556ad40f7
root <root@ip-10-0-25-47.ap-southeast-3.compute.internal>
2023-02-24 09:24:23 +0000

    trial1

---
+++ data/web/inc/app_info.inc.php
@@ -0,0 +1,11 @@
+<?php
+  $MAILCOW_GIT_VERSION="2023-02a";
+  $MAILCOW_LAST_GIT_VERSION="";
+  $MAILCOW_GIT_OWNER="mailcow";
+  $MAILCOW_GIT_REPO="mailcow-dockerized";
+  $MAILCOW_GIT_URL="https://github.com/mailcow/mailcow-dockerized";
+  $MAILCOW_GIT_COMMIT="d20df7d73eccfb13fc8c71bf74552eda27a9c6c1";
+  $MAILCOW_GIT_COMMIT_DATE="2023-02-17 15:52:53 +0100";
+  $MAILCOW_BRANCH="master";
+  $MAILCOW_UPDATEDAT=1677230583;
+?>
